ce7e0bb0a257c70c9f5da89caab0d1380bf5390f
jaehyeonjeong <definejae234@gmail.com>
2023-08-31 12:34:29 +0900

    2023-08-31

---
+++ Chap03/8_Thread/main8-2.py
@@ -0,0 +1,17 @@
+import threading
+import time
+
+def thread_1():
+    while True:
+        print("쓰레드1 동작")
+        time.sleep(1.0)
+
+t1 = threading.Thread(target=thread_1)
+t1.daemon = True
+t1.start()
+
+while True:
+    print("메인동작")
+    time.sleep(2.0)
+
+#쓰레드를 데몬쓰레드로 설정하여 메인동작이 실행될 때만 쓰레드를 실행하도록 함.
