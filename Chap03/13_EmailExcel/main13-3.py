aebd2c18d2ace65976d101d55987bf1f9f132e37
jaehyeonjeong <definejae234@gmail.com>
2023-09-14 20:29:35 +0900

    2023-09-14

---
+++ Chap03/13_EmailExcel/main13-3.py
@@ -0,0 +1,19 @@
+import requests
+import re
+
+url = 'https://news.v.daum.net/v/20211129144552297'
+
+headers = {
+    'User-Agent' : 'Mozilla/5.0',
+    'Content-Type' : 'text/html; charset=utf-8'
+}
+
+response = requests.get(url, headers=headers)
+
+results = re.findall(r'[\w\.-]+@[\w\.-]+', response.text)
+
+results = list(set(results))
+
+print(results)
+
+#사이트에서 이메일 수집하는 코드 만들기
